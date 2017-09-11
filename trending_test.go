36d852d8bfec92c719f7e57491216be3ab98ec0b
Neri Marschik <codesuki@users.noreply.github.com>
2017-09-11 09:36:06 +0900

    Remove memory test

--- trending_test.go
+++ trending_test.go
@@ -1,10 +1,6 @@
 package trending
 
-import (
-	"fmt"
-	"testing"
-	"time"
-)
+import "testing"
 
 func TestNewScorer(t *testing.T) {
 
@@ -15,23 +11,9 @@ func TestAddEvent(t *testing.T) {
 }
 
 func TestScore(t *testing.T) {
-	// check that current count is not used in total
 
 }
 
 func TestMemory(t *testing.T) {
-	scorer := NewScorer()
 
-	now := time.Now()
-	for i := 0; i < 1000000; i++ {
-		scorer.AddEvent(fmt.Sprintf("%d", i), now)
-	}
-
-	fmt.Println("finished")
-
-	start := time.Now()
-	scorer.Score()
-	fmt.Println("took", time.Since(start))
-
-	time.Sleep(1000 * time.Second)
 }
